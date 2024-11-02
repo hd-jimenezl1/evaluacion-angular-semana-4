5c4c1f6cd053b501fdf78f37f87630287b5404a6
Hernan Dario Jimenez <hd.jimenezl1@uniandes.edu.co>
2024-11-02 13:31:33 -0500

    Punto No. 2 - Calcular numero tipo plantas

--- vivero-el-otonio/src/app/planta/planta-list/planta-list.component.ts
+++ vivero-el-otonio/src/app/planta/planta-list/planta-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { PlantaService } from '../planta.service';
 import { Planta } from '../planta';
 
+const TIPO_INTERIOR = "Interior";
+const TIPO_EXTERIOR = "Exterior";
+
 @Component({
   selector: 'app-planta-list',
   templateUrl: './planta-list.component.html',
@@ -12,6 +15,30 @@ export class PlantaListComponent implements OnInit {
   plantas:Planta[] = [];
 
   constructor(private plantaService:PlantaService) { }
+
+  getPlantasInterior():number {
+
+    if(this.plantas) {
+
+      return this.plantas.filter(planta => TIPO_INTERIOR === planta.tipo).length
+    }
+    else
+    {
+      return 0;
+    }
+  }
+
+  getPlantasExterior():number {
+
+    if(this.plantas) {
+
+      return this.plantas.filter(planta => TIPO_EXTERIOR === planta.tipo).length
+    }
+    else
+    {
+      return 0;
+    }
+  }
 
   ngOnInit() {
 
